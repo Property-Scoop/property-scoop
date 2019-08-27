533892f884eb3bb8d159968680ef7ca3099636d5
ludwindl <deleonantigua@gmail.com>
2019-08-26 19:36:40 -0700

    dependencies on server.js

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 'use strict';
-var express = require('express')
-var app = express()
+//application dependencies
+const express = require('express')
+const app = express()
+
+const superagent = require('superagent');
+
+const cors = require('cors');
+app.use(cors());
 
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
+
+//dependency for database
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL)
+client.connect();
+client.on('error', err => console.error(err));
+
+//ejs dependency
+/*app.set('view engine', 'ejs');
+app.use(express.static('./public/../'));*/
 
 app.get('/', function (req, res) {
   res.send('hello world')
