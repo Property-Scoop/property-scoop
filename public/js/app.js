2fc6ebcfca07008278f5dd64f2815753b8b60d5a
Aaron Imbrock <aimbrock@gmail.com>
2019-08-27 23:56:54 -0700

    Geo location api test code and URLs

---
+++ public/js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// https://developers.google.com/web/fundamentals/native-hardware/user-location/
+// check for Geolocation support
+if (navigator.geolocation) {
+console.log('Geolocation is supported!');
+}
+    else {
+    console.log('Geolocation is not supported for this Browser/OS.');
+    };
+
+
+
+button.onclick = function() {
+    var startPos;
+    var nudge = document.getElementById("nudge");
+  
+    var showNudgeBanner = function() {
+      nudge.style.display = "block";
+    };
+  
+    var hideNudgeBanner = function() {
+      nudge.style.display = "none";
+    };
+  
+    var nudgeTimeoutId = setTimeout(showNudgeBanner, 5000);
+  
+    var geoSuccess = function(position) {
+      hideNudgeBanner();
+      // We have the location, don't display banner
+      clearTimeout(nudgeTimeoutId);
+  
+      // Do magic with location
+      startPos = position;
+      document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+      document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    };
+    var geoError = function(error) {
+      switch(error.code) {
+        case error.TIMEOUT:
+          // The user didn't accept the callout
+          showNudgeBanner();
+          break;
+      }
+    };
+  
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  };
+
+  window.onload = function() {
+    var startPos;
+    var geoSuccess = function(position) {
+      startPos = position;
+      document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+      document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    };
+    navigator.geolocation.getCurrentPosition(geoSuccess);
+  };
