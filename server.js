31cc295942d462a6ff3030efd2710ba20139ccf7
nastinsk <nastinsk@gmail.com>
2019-08-28 18:56:34 -0700

    css layout fixed, new logo

--- server.js
+++ server.js
@@ -53,6 +53,27 @@ app.get('/aboutUs', function (req, res) {
 
 //=======================================================================================//
 
+// // constructor function to buld a city object instances, paths based on the geo.json file
+// function City(query, data){
+//   this.search_query = query;
+//   this.formatted_query = data.formatted_address;
+//   this.latitude = data.geometry.location.lat;
+//   this.longitude = data.geometry.location.lng;
+//   this.id;
+// }
+
+// ///prototype function to City constructor function to post NEW data in database
+
+// City.prototype.postLocation = function (){
+
+//   let SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id';
+//   const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
+
+//   return client.query(SQL, values)
+//     .then (result => {
+//       this.id = result.rows[0].id;
+//     });
+// };
 
 
 
