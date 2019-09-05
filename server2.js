58650a1fbe7d0a73fea6835df5765b16a525b148
ludwindl <deleonantigua@gmail.com>
2019-09-04 19:47:36 -0700

    mvp done

--- server2.js
+++ server2.js
@@ -1,7 +1,6 @@
 'use strict';
 
 //=====================Global Variables and appplication dependensies=================================//
-
 require('dotenv').config();
 
 // Load Application Dependencies
@@ -16,9 +15,9 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Connect To Database
-const client = new pg.Client(process.env.DATABASE_URL)
-client.connect();
-client.on('error', err => console.error(err));
+// const client = new pg.Client(process.env.DATABASE_URL)
+// client.connect();
+// client.on('error', err => console.error(err));
 
 //ejs dependency
 app.set('view engine', 'ejs');
@@ -36,8 +35,8 @@ app.get('/', function (req, res) {
 app.get('/aboutUs', function (req, res) {
   res.render('aboutUs');
 })
-app.get('/searchResults', getLocation);
-
+app.get('/searchResults', searchToLatLong);
+//app.get('/searchResults', getLocation);
 //=======================================Constructor Functions===========================//
 // // constructor function to buld a city object instances, paths based on the geo.json file
 function Location(query, res) {
@@ -49,20 +48,24 @@ function Location(query, res) {
   this.id;
 }
 
-
-//function to add location data in database
-Location.prototype.addLocation = function (){
-
-  let SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id';
-  const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
-
-  return client.query(SQL, values)
-    .then (result => {
-      this.id = result.rows[0].id;
-      //console.log(this.id);
-    });
-};
-// Function constructor for kc api
+// {
+//   "identifier": "PIN",
+//   "items": [
+//     {
+//       "PIN": "0695000335",
+//       "TAXPAYERNAME": "SELIG HOLDINGS COMPANY LLC",
+//       "JURISDICTION": "SEATTLE",
+//       "PROPNAME": "THIRD AND BROAD BLDG",
+//       "PRESENTUSE": "Office Building",
+//       "LEVYCODE": "0011",
+//       "ADDRESS": "2901 3RD AVE 98121",
+//       "APPVALUE": "73977000",
+//       "NUMBUILDINGS": "",
+//       "NUMUNITS": "",
+//       "LOTSQFT": "63710"
+//     }
+//   ]
+// }
 function Property(property) {
   this.PIN = property.PIN;
   this.taxpayerName = property.TAXPAYERNAME;
@@ -77,7 +80,18 @@ function Property(property) {
   this.lotSqft = property.LOTSQFT;
 }
 
+//function to add location data in database
+// Location.prototype.addLocation = function (){
 
+//   let SQL = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id';
+//   const values = [this.search_query, this.formatted_query, this.latitude, this.longitude];
+
+//   return client.query(SQL, values)
+//     .then (result => {
+//       this.id = result.rows[0].id;
+//       console.log(this.id);
+//     });
+// };
 //=======================================================================================//
 
 
@@ -85,29 +99,27 @@ function Property(property) {
 //=======================================Functions========================================//
 // ERROR HANDLER
 function handleError(err, res) {
-  console.error(err);
   if (res) res.status(500).send('Sorry, something went wrong');
 }
 
 function searchToLatLong(request, response) {
-  //Define the URL for the GEOCODE API
+  //Define the first URL for the GEOCODE API
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.search}&key=${process.env.GEOCODE_API_KEY}`;
-
+  let propertydata;
   superagent.get(url)
     .then(result => {
+      //console.log('###########', result.body.results[0].formatted_address);
       const location = new Location(request.query.search, result);
       const cleanedAddress = cleanAddress(result.body.results[0].formatted_address);
-      location.addLocation(request);
+      // location.addLocation(request);
       let urlGIS = encodeURIComponent(cleanedAddress)
       getKingCountyGISdata(urlGIS)
         .then(thing => {
-          response.render('searchResults', {locationData: `https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap\&markers=size:mid%7Ccolor:red%7C${location.latitude}%2c%20${location.longitude}&key=${process.env.GEOCODE_API_KEY}`,
-            address:location.formatted_query,
-            location: location, propertyData: thing
-          });
-        })
-        .catch(err => {handleError(err, response)})
+          response.render('searchResults', {locationData: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude}%2c%20${location.longitude}&zoom=17&size=600x300&maptype=roadmap
+        &key=${process.env.GEOCODE_API_KEY}`, address:location.formatted_query, propertyData: thing})})
+      console.log('Property inside Lat Long: ', propertydata);
     })
+    .catch(err => {handleError(err, response)})
 }
 
 function cleanAddress(address) {
@@ -119,6 +131,45 @@ function cleanAddress(address) {
   }
   return cleaned.join(' ');
 }
+//=======================================================================================//
+
+
+
+//=======================================Ana's functions=================================//
+
+// //route to handle user request and send the response from our database or GOOGLE
+// function getLocation(req,res){
+
+//   //check if this lcoation exist in database
+//   //lookupLocation(req.query.search)
+//     .then(location => {
+
+//       if (location){
+//         //if exists send the object as response
+//         res.render('searchResults', {locationData: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude}%2c%20${location.longitude}&zoom=17&size=600x300&maptype=roadmap
+//       &key=${process.env.GEOCODE_API_KEY}`, address:location.formatted_query});
+//       }
+
+//       //if doesn't exists go to go to google api
+//       else
+//       {
+//         searchToLatLong(req, res);
+//       }
+//     });
+// }
+
+//check if data from SQL DB contains requested location
+// let lookupLocation = (location) =>{
+//   let SQL = 'SELECT * FROM locations WHERE search_query=$1';
+//   let values = [location];
+//   return client.query(SQL, values)
+//     .then(result => {
+//       if (result.rowCount > 0){
+//         // if so return location data
+//         return result.rows[0];
+//       }
+//     });
+// };
 
 function getKingCountyGISdata(location) {
   let getPIN = `https://gismaps.kingcounty.gov/parcelviewer2/addSearchHandler.ashx?add=${location}`;
@@ -147,52 +198,6 @@ function getKingCountyGISdata(location) {
     })
 }
 
-
-//=======================================Ana's functions=================================//
-
-//route to handle user request and send the response from our database or GOOGLE
-function getLocation(req,res){
-
-  //check if this lcoation exist in database
-  lookupLocation(req.query.search)
-    .then(location => {
-      //if exists send the object as response
-
-      if (location){
-        const cleanedAddress = cleanAddress(location.formatted_query);
-        // location.addLocation(request);
-        let urlGIS = encodeURIComponent(cleanedAddress)
-        getKingCountyGISdata(urlGIS)
-          .then(thing => {
-            res.render('searchResults', {locationData:  `https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap\&markers=size:mid%7Ccolor:red%7C${location.latitude}%2c%20${location.longitude}&key=${process.env.GEOCODE_API_KEY}`, address:location.formatted_query, propertyData: thing, location: location})})
-      }
-
-      //if doesn't exists go to go to google api
-      else
-      {
-        searchToLatLong(req, res);
-      }
-    });
-}
-
-//check if data from SQL DB contains requested location
-let lookupLocation = (location) =>{
-  let SQL = 'SELECT * FROM locations WHERE search_query=$1';
-  let values = [location];
-  return client.query(SQL, values)
-    .then(result => {
-      if (result.rowCount > 0){
-        // if so return location data
-        return result.rows[0];
-      }
-    });
-};
-
-
-// add  building to database from search form
-// fix the order and value names . and in form we will have only note al other things will be as a paragraphs
-
-
 // function postBuilding(request, response){
 
 //   const SQL = `INSERT INTO buildings(image_url, owner, permit_num, year, description, value, note, sq_feet) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`;
@@ -211,4 +216,4 @@ let lookupLocation = (location) =>{
 // }
 
 
-//=======================================================================================//
+//=======
